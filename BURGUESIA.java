e91a4bb7a88c6c1fdc473ad42472122ca2b1439d
llucasBorba <99662060+llucasBorba@users.noreply.github.com>
2023-03-31 11:19:56 -0300

    Add files via upload

---
+++ BURGUESIA.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class BURGUESIA {
+
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+     
+    System.out.println("Digite o nome: ");
+    String vNome = sc.nextLine();
+    
+    
+    System.out.println("Digite a idade: ");
+    int vidade = sc.nextInt();
+    
+    System.out.println("Digite a salario: ");
+    int vsalario = sc.nextInt();
+    
+    System.out.println("Nome: " +vNome
+                        +"Idade: " +vidade
+                         +"Salario: "+vsalario);
+  
+    
+   /*
+   String nome;
+    JOptionPane.showInputDialog(null, "Digite o nome: ");
+    
+    
+    
+    
+    
+    JOptionPane.showMessageDialog(null,"O nome é:" +nome);
+    */
+    }} 
